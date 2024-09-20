d2e49fa67bb3bf659eac35a028cab0c9f999f0b8
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-09-20 18:22:59 -0300

    refactor: removing main logic from serialCommands and using myCallbacks instead

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int machine_state = 0;
+
 void stringFormatT(char* buffer, const char *format, ...) {
     va_list args;
     va_start(args, format);
@@ -20,16 +22,44 @@ void stringFormatT(char* buffer, const char *format, ...) {
 // Servo
 // ----------------------------------------------------------------------------------- //
 Servo servo_x_machine;
-int servo_x_pin = 18;
+const int servo_x_pin = 18;
 
 Servo servo_y_machine;
-int servo_y_pin = 19;
+const int servo_y_pin = 19;
 
-int joystick_pin_x   = A0; // GPIO 36
-int joystick_pin_y   = A10; // GPIO 4
+const int joystick_pin_x  = A0; // GPIO 36
+const int joystick_pin_y  = A10; // GPIO 4
+const int joystick_pin_sw = 33; // D33
 
 int state_y_position = 0;
 int state_x_position = 90;
+
+int lower_limit_x = -1;
+int lower_limit_y = -1;
+
+int upper_limit_x = -1;
+int upper_limit_y = -1;
+
+int sw_count = 0;
+
+int isSwPressed () {
+    int value = analogRead(joystick_pin_sw);
+    if(!value) {
+        delay(1807);
+    }
+
+    value = analogRead(joystick_pin_sw);
+
+    return value;
+}
+
+int isLowerLimitSet() {
+    return lower_limit_x != -1 && lower_limit_y != -1;
+}
+
+int isUpperLimitSet() {
+    return upper_limit_x != -1 && upper_limit_y != -1;
+}
 
 void moveJoystick() {
     int map_volts_x_value = analogRead(joystick_pin_x);
@@ -80,7 +110,7 @@ BLECharacteristic* pCharacteristic = NULL;
 bool oldDeviceConnected = false;
 bool deviceConnected = false;
 
-char msg[20] = "Initial Message";
+char msg[20] = "";
 
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic* pCharacteristic) {
@@ -94,6 +124,32 @@ class MyCallbacks: public BLECharacteristicCallbacks {
             }
 
             Serial.println();
+
+            if (machine_state == 0) {
+                if (value.compare("a") == 0) {
+                    lower_limit_x = state_x_position;
+                    lower_limit_y = state_y_position;
+
+                    pCharacteristic->setValue("lower");
+                    pCharacteristic->notify();
+                }
+
+                if (value.compare("b") == 0) {
+                    upper_limit_x = state_x_position;
+                    upper_limit_y = state_y_position;
+
+                    pCharacteristic->setValue("upper");
+                    pCharacteristic->notify();
+
+                    machine_state = 1;
+                }
+
+                Serial.printf("%d,%dX%d,%d\n", lower_limit_x, lower_limit_y, upper_limit_x, upper_limit_y);
+            }
+
+            if (machine_state == 1) {
+                Serial.printf("second state\n");
+            }
         }
     }
 };
@@ -108,18 +164,55 @@ class MyServerCallbacks: public BLEServerCallbacks {
     }
 };
 
+// Unused
+// Look for MyCallback class
 void serialCommands() {
   if(Serial.available()) {
     String cmd = Serial.readString();
+    char coordinates[20];
     
     cmd.toCharArray(msg, cmd.length());
-    Serial.print("Writed to APP : ");
+    Serial.print("Received from app: ");
+    Serial.println(msg);
+
+    // set lower if not
+    // set upper if lower setted
+    if(!isLowerLimitSet()) {
+        lower_limit_x = state_x_position;
+        lower_limit_y = state_y_position;
+    }
+
+    if(!isUpperLimitSet()) {
+        upper_limit_x = state_x_position;
+        upper_limit_y = state_y_position;
+    }
+
+    stringFormatT(msg, "%d,%dX%d,%d", lower_limit_x, lower_limit_y, upper_limit_x, upper_limit_y);
     Serial.println(msg);
     pCharacteristic->setValue(msg);
     pCharacteristic->notify();
   }
 }
 
+void bleConn() {
+    // notify changed value
+    if (deviceConnected) {
+        delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+    }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        Serial.println("connected");
+        oldDeviceConnected = deviceConnected;
+    }
+}
 // ----------------------------------------------------------------------------------- //
 
 void setup() {
@@ -163,27 +256,6 @@ void setup() {
 }
 
 void loop() {
-    serialCommands();
+    bleConn();
     moveJoystick();
-    
-    // notify changed value
-    if (deviceConnected) {
-        stringFormatT(msg, "X%dY%d", state_x_position, state_y_position);
-        pCharacteristic->setValue(msg);
-        pCharacteristic->notify();
-        delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-    }
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
-        Serial.println("connected");
-        oldDeviceConnected = deviceConnected;
-    }
 }
